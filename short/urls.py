1772e1659eb27cb3cbb1c6cfaeca9fb1b8870858
adii-10 <149268706+adii-10@users.noreply.github.com>
2024-07-22 21:33:43 +0530

    Update urls.py

--- short/urls.py
+++ short/urls.py
@@ -4,5 +4,6 @@ from short import views
 urlpatterns = [
     path('', views.head, name='head'),
     path('create',views.create,name='create'),
+    path('favicon.ico', views.favicon_view),
     path('<str:pk>', views.go, name='go')
 ]
